a8f39e8be83ee213367435bc96b04dbc77076448
RandomBlueGuy <randombluemail@gmail.com>
2023-05-25 09:08:42 -0500

    Add complete basic User CRUD

--- src/api/admin/admin.controller.js
+++ src/api/admin/admin.controller.js
@@ -84,29 +84,43 @@ module.exports = {
   //GET USER INFO
   async getUserData(req, res) {
     try {
-      // if (req.body.secretKey === process.env.ACCESS_KEY) {
-      //   throw new Error("INVALID ACCESS!");
-      // }
+      if (req.body.secretKey !== process.env.ACCESS_KEY) {
+        throw new Error("INVALID ACCESS!");
+      }
 
       const users = await Admin.find();
 
-      res.status(201).json({ message: "Success", users });
-    } catch (err) {
-      res.status(400).json(err);
+      res.status(201).json({ message: "Success!", users });
+    } catch (error) {
+      res.status(400).json(error.message);
     }
   },
 
   // UPDATE USER
   async updateUserInfo(req, res) {
     try {
+      let isDeleted = false;
       const { userId } = req.params;
-      const updatedData = req.body;
+      const encPassword = await bcrypt.hash(req.body.password, 8);
+
+      const updatedData = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        userName: req.body.userName,
+        password: encPassword,
+        location: req.body.location,
+        profilePicture: req.body.profilePicture,
+      };
+
+      if (req.body.secretKey !== process.env.ACCESS_KEY) {
+        throw new Error("INVALID ACCESS!");
+      }
 
       const updatedUser = await Admin.findByIdAndUpdate(userId, updatedData, {
         new: true,
       });
 
-      if (!user) {
+      if (!updatedUser) {
         return res
           .status(404)
           .json({ message: `There is no user with this Id inside the DB` });
@@ -117,6 +131,35 @@ module.exports = {
       res.status(400).json({
         message: "Sorry! We couldn't update the user",
       });
+    }
+  },
+
+  async deleteUser(req, res) {
+    try {
+      const { userId } = req.params;
+
+      if (req.body.secretKey !== process.env.ACCESS_KEY) {
+        throw new Error("INVALID ACCESS!");
+      }
+
+      // Admin.findByIdAndDelete(
+      //   userId,
+      //   (isDeleted = (error) => {
+      //     return error ? false : true;
+      //   })
+      // );
+
+      const result = await Admin.findByIdAndDelete(userId);
+      const isDeleted = result ? true : false;
+
+      console.log(
+        "🔷 / file: admin.controller.js:155 / deleteUser / isDeleted =>",
+        isDeleted
+      );
+
+      res.status(200).json({ message: `User deleted successfully` });
+    } catch (error) {
+      res.status(400).json(error.message);
     }
   },
 };
